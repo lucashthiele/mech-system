57eb9240fe29efa10357e54df8965187fffee841
lucashthiele <lucasthiele0@outlook.com>
2023-10-02 19:14:02 -0300

    init

---
+++ src/main/java/com/lucashthiele/mechsystem/MechSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lucashthiele.mechsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucashthiele/mechsystem/MechSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucashthiele.mechsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
